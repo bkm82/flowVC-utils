6ee8c916f4a9aeec0e49a0d9556e2ed67bd38aae
Bray Moll <bkm82@nau.edu>
2024-12-10 22:53:46 -0700

    Revert "Readme Update (#4)"
    This reverts commit 08c329716f8ffe0af1348236f6782f3f921df565.

--- src/flowvcutils/filerename.py
+++ src/flowvcutils/filerename.py
@@ -52,9 +52,7 @@ def main(root, filename, prefix):
 if __name__ == "__main__":
     settup_logging()
     # Parse a CLI flag to enable setting the log level from the CLI
-    parser = argparse.ArgumentParser(
-        description="Rename the file names in a directory."
-    )
+    parser = argparse.ArgumentParser(description="Process VTU files to a .bin format.")
     parser.add_argument(
         "--root",
         default=os.getcwd(),
