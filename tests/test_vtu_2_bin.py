d2df45da56d1ec8e9b46f681b01140f6e26c2e1f
bkm82 <130317980+bkm82@users.noreply.github.com>
2024-12-11 03:00:26 -0700

    5 add a  vel to the generated velocity bin file (#6)
    * add create_vel_file_name testcase
    
    * refactor: split output into directory and filename
    
    * refactor: split velocity input into directory and filename
    
    * bugfix: add missing argument
    
    * bugfix: fix the vtu to bin function call (input directory)
    
    * wip: vtu_2_bin use create_file_path function.
    
    * refactor: fix linting issue from too long of a comment
    
    * refactor: vtu_2_bin move main function calls to a function
    
    * refactor: move arg parsing to main instead of __main__
    
    * remove positional args from main
    
    * update test to hopefully work with windows
    
    * update test for windows

--- tests/test_vtu_2_bin.py
+++ tests/test_vtu_2_bin.py
@@ -1,8 +1,15 @@
 import vtk
 import pytest
+import os
 from unittest.mock import MagicMock
-from flowvcutils.vtu_2_bin import reader_selection
-from flowvcutils.vtu_2_bin import coordinates_file
+import tempfile
+from flowvcutils.vtu_2_bin import (
+    reader_selection,
+    coordinates_file,
+    create_vel_file_path,
+    strip_trailing_underscore,
+    create_file_path,
+)
 
 
 @pytest.mark.parametrize(
@@ -45,3 +52,67 @@ def test_coordinates_shape(mock_data):
     obj = coordinates_file(mock_data)
     obj.create_file()
     assert obj.coordinates.shape == (6,)
+
+
+@pytest.mark.parametrize(
+    "file_name, expected", [("test_name", "test_name"), ("test_name_", "test_name")]
+)
+def test_strip_trailing_underscore(file_name, expected):
+    assert strip_trailing_underscore(file_name) == expected
+
+
+@pytest.mark.parametrize(
+    "file_name, file_num, expected",
+    [
+        ("file_name_", 50, "file_name_vel.50.bin"),
+        ("file_name", 50, "file_name_vel.50.bin"),
+    ],
+)
+def test_create_vel_file_path(file_name, file_num, expected):
+    temp_dir = tempfile.mkdtemp()
+
+    expected_path = os.path.join(temp_dir, expected)
+    assert create_vel_file_path(temp_dir, file_name, file_num) == expected_path
+
+
+@pytest.mark.parametrize(
+    "file_name, file_type, expected",
+    [
+        (
+            "file_name_",
+            "coordinates",
+            "file_name_coordinates.bin",
+        ),
+        (
+            "file_name",
+            "coordinates",
+            "file_name_coordinates.bin",
+        ),
+        (
+            "file_name_",
+            "adjacency",
+            "file_name_adjacency.bin",
+        ),
+        (
+            "file_name",
+            "adjacency",
+            "file_name_adjacency.bin",
+        ),
+        (
+            "file_name_",
+            "connectivity",
+            "file_name_connectivity.bin",
+        ),
+        (
+            "file_name",
+            "connectivity",
+            "file_name_connectivity.bin",
+        ),
+    ],
+)
+def test_create_file_path(file_name, file_type, expected):
+    temp_dir = tempfile.mkdtemp()
+
+    expected_path = os.path.join(temp_dir, expected)
+    actual = create_file_path(root=temp_dir, file_name=file_name, file_type=file_type)
+    assert actual == expected_path
