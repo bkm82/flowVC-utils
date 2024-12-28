cc636c179f152fbb1dd62cf62f7ba256eb5d43ce
Bray Moll <bkm82@nau.edu>
2024-12-27 23:05:55 -0700

    wip: use exclude.txt in simulation generator

--- src/flowvcutils/simulationgenerator.py
+++ src/flowvcutils/simulationgenerator.py
@@ -7,6 +7,26 @@ from flowvcutils.jsonlogger import settup_logging
 logger = logging.getLogger(__name__)
 
 
+def create_directories(base_dir):
+    inlet_velocity_directory = os.path.join(base_dir, "inlet_velocity")
+    generic_dir = os.path.join(base_dir, "generic_file")
+
+    # populate an exclude files list
+    exclude_files = []
+    exclude_files_path = os.path.join(inlet_velocity_directory, "exclude.txt")
+    if os.path.isfile(exclude_files_path):
+        with open(exclude_files_path, "r") as exclude:
+            exclude_files = [line.strip() for line in exclude if line.strip()]
+
+    for txt_file in os.listdir(inlet_velocity_directory):
+        if txt_file.endswith(".txt") and txt_file not in exclude_files:
+            file_name_base = os.path.splitext(txt_file)[0]
+
+            # Step 1: Copy generic_file and rename it
+            new_dir_path = os.path.join(base_dir, file_name_base)
+            shutil.copytree(generic_dir, new_dir_path)
+
+
 def process_files(base_dir, exclude_files):
     """
     Coppies the generic directory and updates inlet velocity"
