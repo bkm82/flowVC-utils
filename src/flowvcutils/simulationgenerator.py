9f14f42ac44699e42d6e3c2a1f3509031c137716
bkm82 <130317980+bkm82@users.noreply.github.com>
2024-12-28 00:52:42 -0700

    Simulationgenerator (#16)
    * wip
    
    * add simulation generatory (without running svpre)
    
    * add svpre command execution
    
    * escape venv when running svpre
    
    * fix svsolver file path
    
    * fix linting
    
    * add test for simulation_generator cli
    
    * wip: use exclude.txt in simulation generator
    
    * refactor: move get_exclude_files to a standalone function
    
    * wip
    
    * move everything over to tested create_directories function
    
    * create a numstart.dat file if it does not exist
    
    * refactor: create numstart file into its own function
    
    * fix linting issue
    
    * add a test calling the main function
    
    * refactor test order
    
    * remove unused main.py and associated tests

---
+++ src/flowvcutils/simulationgenerator.py
@@ -0,0 +1,97 @@
+import os
+import shutil
+import logging
+import subprocess
+from flowvcutils.jsonlogger import settup_logging
+
+logger = logging.getLogger(__name__)
+
+
+def create_directories(base_dir, exclude_files=None):
+    inlet_velocity_directory = os.path.join(base_dir, "inlet_velocity")
+    generic_dir = os.path.join(base_dir, "generic_file")
+
+    # populate an exclude files list
+    exclude_file_path = os.path.join(inlet_velocity_directory, "exclude.txt")
+    exclude_files = get_exclude_files(exclude_file_path, exclude_files)
+
+    for txt_file in os.listdir(inlet_velocity_directory):
+        if txt_file.endswith(".txt") and txt_file not in exclude_files:
+            txt_file_path = os.path.join(inlet_velocity_directory, txt_file)
+            file_name_base = os.path.splitext(txt_file)[0]
+
+            # Step 1: Copy generic_file and rename it
+            new_dir_path = os.path.join(base_dir, file_name_base)
+            shutil.copytree(generic_dir, new_dir_path)
+
+            # Step 2: Replace catheter.flow with the contents of the txt file
+            catheter_flow_path = os.path.join(new_dir_path, "catheter.flow")
+            replace_file(source=txt_file_path, destination=catheter_flow_path)
+
+            # Step 3: Copy and rename generic_file.sjb
+            generic_file_sjb = os.path.join(base_dir, "generic_file.sjb")
+            new_sjb_file = os.path.join(base_dir, f"{file_name_base}.sjb")
+            shutil.copy2(generic_file_sjb, new_sjb_file)
+
+            # Step 4: Create a numstart.dat with a 0 if it does not exist
+            create_numstart_file(new_dir_path)
+
+            # Step 5: Run the svpre command
+            command = "/usr/local/sv/svsolver/2022-07-22/bin/svpre generic_file.svpre"
+            run_command(command=command, path=new_dir_path)
+
+
+def replace_file(source, destination):
+    with open(source, "r") as source_content, open(
+        destination, "w"
+    ) as destination_file:
+        destination_file.write(source_content.read())
+
+
+def get_exclude_files(exclude_file_path, exclude_files=None):
+    """
+    Reads the exclude.txt file in the inlet_velocity directory
+    and returns a list of files to exclude. Optionally,
+    it can append to an existing exclude_files list.
+
+    Args:
+        base_dir (str): Base directory containing the inlet_velocity directory.
+        exclude_files (list, optional): List of files to exclude. Defaults to None.
+
+    Returns:
+        list: Updated list of exclude files.
+    """
+    if exclude_files is None:
+        exclude_files = []
+    if os.path.isfile(exclude_file_path):
+        with open(exclude_file_path, "r") as ef:
+            exclude_files += [line.strip() for line in ef if line.strip()]
+
+    return exclude_files
+
+
+def create_numstart_file(new_dir_path):
+    """
+    Creates numstart.dat in the specified directory if it doesn't exist.
+    Writes a 0 to the file.
+
+    Args:
+        new_dir_path (str): The directory where the numstart.dat will be created.
+    """
+    numstart_file_path = os.path.join(new_dir_path, "numstart.dat")
+
+    # Check if the file exists
+    if not os.path.isfile(numstart_file_path):
+        with open(numstart_file_path, "w") as numstart_file:
+            numstart_file.write("0")
+
+
+def run_command(command, path):
+    subprocess.run(command, shell=True, cwd=path, env=os.environ)
+
+
+def main(directory, exclude):
+    settup_logging()
+    logger.info("Creating Simulation Directories")
+    create_directories(base_dir=directory, exclude_files=exclude)
+    logger.info("Simulation Directories Created")
---
+++ tests/test_simulationgenerator.py
@@ -0,0 +1,101 @@
+import os
+import pytest
+from unittest.mock import patch
+from tempfile import TemporaryDirectory
+from flowvcutils import simulationgenerator
+
+
+@pytest.fixture
+def setup_test_environment(tmp_path):
+    with TemporaryDirectory() as tmp_dir:
+        base_dir = tmp_dir
+
+        # base_dir.mkdir()
+        os.makedirs(os.path.join(base_dir, "generic_file"))
+        inlet_velocity_dir = os.path.join(base_dir, "inlet_velocity")
+        os.makedirs(inlet_velocity_dir)
+        # Add a steady.txt file
+        steady_file_path = os.path.join(inlet_velocity_dir, "steady_.txt")
+        with open(steady_file_path, "w") as f:
+            f.write("0.0 -4.16e-05\n1.01400006 -4.16e-05\n")
+
+        # Add an exclude.txt file
+        exclude_file_path = os.path.join(inlet_velocity_dir, "exclude.txt")
+        with open(exclude_file_path, "w") as f:
+            f.write("exclude.txt\n")
+        generic_file_path = os.path.join(base_dir, "generic_file.sjb")
+        with open(generic_file_path, "w"):
+            pass
+        yield base_dir
+
+
+def test_create_directories(setup_test_environment):
+    """Test that the copy_directory works."""
+    src = setup_test_environment
+    dest = os.path.join(setup_test_environment, "steady_")
+    simulationgenerator.create_directories(src)
+    assert os.path.exists(dest)
+
+
+def test_create_directories_exclude(setup_test_environment):
+    """Test that the create_dirctorys respects exclude.txt."""
+    src = setup_test_environment
+    dest = os.path.join(setup_test_environment, "exclude")
+    simulationgenerator.create_directories(src)
+    assert not os.path.exists(dest)
+
+
+def test_create_sjb_file(setup_test_environment):
+    """Test that the create_dirctorys creates a steady_.sjb file"""
+    src = setup_test_environment
+    dest = os.path.join(setup_test_environment, "steady_.sjb")
+    simulationgenerator.create_directories(src)
+    assert os.path.isfile(dest)
+
+
+def test_create_flow_file(setup_test_environment):
+    """Test that the create_dirctorys creates a catheter.flow file"""
+    src = setup_test_environment
+    dest = os.path.join(setup_test_environment, "steady_", "catheter.flow")
+    expected = "0.0 -4.16e-05\n1.01400006 -4.16e-05\n"
+    simulationgenerator.create_directories(src)
+
+    with open(dest, "r") as f:
+        actual = f.read()
+
+    assert actual == expected
+
+
+@patch("flowvcutils.simulationgenerator.subprocess.run")
+def test_run_command(mock_run):
+    simulationgenerator.run_command("echo test", "/path/to/cwd")
+    mock_run.assert_called_once_with(
+        "echo test", shell=True, cwd="/path/to/cwd", env=os.environ
+    )
+
+
+def test_main_valid_directory(setup_test_environment, capsys):
+    import flowvcutils.simulationgenerator as sg
+
+    # Call main with the setup test environment
+    sg.main(str(setup_test_environment), exclude=None)
+
+    # Capture output to check logging/message
+    captured = capsys.readouterr()
+    assert "Creating Simulation Directories" in captured.out
+    assert "Simulation Directories Created" in captured.out
+
+
+# Regression Test
+# Ensure a numstart.dat file exists
+def test_numstart_dat(setup_test_environment):
+    """Test that the numstart.dat file exists with a 0"""
+    src = setup_test_environment
+    dest = os.path.join(setup_test_environment, "steady_", "numstart.dat")
+    expected = "0"
+    simulationgenerator.create_directories(src)
+
+    with open(dest, "r") as f:
+        actual = f.read()
+
+    assert actual == expected
